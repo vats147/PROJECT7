c8024a59e88fa263fb129ddf23c7a2d83f7dfa5a
vats <vatsalmangukiya9003@gmail.com>
2023-10-02 16:20:18 +0530

    Redirect To Login & 404 Page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
 import Row1 from "./Components/Row1";
 import Stud from "./Components/Stud";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from "./Components/NotFound";
+import { BrowserRouter as Router, Routes, Route,  Navigate } from 'react-router-dom';
 import Login from "./Components/Login";
 //import Regotp from "./Components/Regotp";
 import Addmenu from "./Components/Addmenu";
 import { useEffect } from "react";
 
+
+
 function App() {
 
   let userLogged;
-  
+
   useEffect(() => {
     function checkUserData() {
       userLogged = localStorage.getItem("login");
-      if(!userLogged)
-      {
+      
+      if (!userLogged) {
         console.log("User not logged in ")
 
-        window.location.href="/Login";
+        window.location.href = "/Login";
       }
 
     }
@@ -31,14 +34,13 @@ function App() {
     };
   }, []);
 
-
   return (
     <>
-    {userLogged ? (
-      // After Login page
+      {userLogged ? (
+        // is user login then --> Dashboard Page 
 
         // Old code
-          <Router>
+        <Router>
           <div className="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
             data-sidebar-position="fixed" data-header-position="fixed">
             <Navbar></Navbar>
@@ -46,62 +48,69 @@ function App() {
               <Header></Header>
               <Routes>
                 <Route path="/Stud" element={<Stud></Stud>}></Route>
-  
+
                 {userLogged ? (
                   //Dashboard
                   // logout route
                   <Route path="/Stud" element={<Stud></Stud>}></Route>
-  
+
                 ) : (
                   <>
-  
+
                     <Route path="/Stud" element={<Stud></Stud>}></Route>
                     <Route path="/Dash" element={<Row1></Row1>}></Route>
-  
+
                     <Route path="/AddMenu" element={<Addmenu></Addmenu>}></Route>
                     <Route path="/Login" element={<Login></Login>}></Route>
                   </>
                 )}
-  
+
               </Routes>
-  
+
             </div>
           </div>
         </Router>
-    ):(
-      <Router>
-      
-       {/* Only Login Page  */}
-        
+      ) : (
 
-        <div className="body-wrapper">
-          
-          <Routes>
-            <Route path="/Stud" element={<Stud></Stud>}></Route>
+        <Router>
 
-            {userLogged ? (
-              //Dashboard
-              // logout route
-              <Route path="/Stud" element={<Stud></Stud>}></Route>
+          {/* If user not login --> Then login.js page */}
+          {/* Display Login   */}
 
-            ) : (
-              <>
 
+          <div className="body-wrapper">
+
+            <Routes>
+              <Route path="/Stud" element={userLogged? <Stud/> : <Navigate to='/Login'/>}>
+                
+              </Route>
+
+              {userLogged ? (
+                //Dashboard
+                // logout route
                 <Route path="/Stud" element={<Stud></Stud>}></Route>
-                <Route path="/Dash" element={<Row1></Row1>}></Route>
 
-                <Route path="/AddMenu" element={<Addmenu></Addmenu>}></Route>
-                <Route path="/Login" element={<Login></Login>}></Route>
-              </>
-            )}
+              ) : (
+                <>
 
-          </Routes>
 
-        </div>
-      
-    </Router>
-    )}
-      
+                  <Route path="/Stud" element={<Stud></Stud>}>
+                    
+                  </Route>
+                  <Route path="/Dash" element={<Row1></Row1>}></Route>
+
+                  <Route path="/AddMenu" element={<Addmenu></Addmenu>}></Route>
+                  <Route path="/Login" element={<Login></Login>}></Route>
+                </>
+              )}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+
+          </div>
+
+        </Router>
+      )}
+
       <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
       <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
       <script src="../assets/js/sidebarmenu.js"></script>
---
+++ src/Components/NotFound.js
@@ -0,0 +1,17 @@
+// import Navbar from "./Components/Navbar";
+// import Header from "./Components/Header";
+// import Row1 from "./Components/Row1";
+// import Stud from "./Components/Stud";
+// import { BrowserRouter as Router, Routes, Route,Switch } from 'react-router-dom';
+// import Login from "./Components/Login";
+// //import Regotp from "./Components/Regotp";
+// import Addmenu from "./Components/Addmenu";
+// import { useEffect } from "react";
+import React from "react";
+function App() {
+       return(
+              <h1>Not Found</h1>
+       );
+}
+
+export default App;
